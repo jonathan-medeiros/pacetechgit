be97ea115ffb8bfae81afb59e1f2f92b11711884
Jonathan Medeiros <jonathan.d.medeiros@gmail.com>
2022-08-05 14:33:43 -0300

    Ajustes na classe Útil

--- src/main/java/com/mycompany/pacetech/git/Util.java
+++ src/main/java/com/mycompany/pacetech/git/Util.java
@@ -10,4 +10,10 @@ package com.mycompany.pacetech.git;
  */
 public class Util {
     
+    
+    public static void main(String[] args){
+        
+        System.out.println("Novo método");
+    }
+    
 }
