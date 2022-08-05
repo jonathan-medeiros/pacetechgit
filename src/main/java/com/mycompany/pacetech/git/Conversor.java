1c96f84c768aa1c3ed405a0cc02037f32435e71d
Jonathan Medeiros <jonathan.d.medeiros@gmail.com>
2022-08-04 23:19:48 -0300

    Add Classe Conversora

---
+++ src/main/java/com/mycompany/pacetech/git/Conversor.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pacetech.git;
+
+/**
+ *
+ * @author jonathandamasiomedeiros
+ */
+public class Conversor {
+    //Esta é a classe conversora.
+    
+}
